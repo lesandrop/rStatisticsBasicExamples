3d2de2f37e585ca3ccf8700c3b86f4f9d03d942b
Lesandro Ponciano <lesandropcodes@users.noreply.github.com>
2021-03-24 20:51:02 -0300

    Update
    Co-Authored-By: Lesandro Ponciano <2512365+lesandrop@users.noreply.github.com>
    Co-Authored-By: Lesandro Ponciano <7714190+lesandropcodes@users.noreply.github.com>

--- codes/ggplot2GraphicalAnalysis.R
+++ codes/ggplot2GraphicalAnalysis.R
@@ -6,6 +6,6 @@ df_activity <- data.frame(Rank=c(1,10,100,1000,10000,1000000,10000000,100000000,
 pl <- ggplot(df_activity,aes(x = Rank, y=Frequency)) +
   scale_x_log10(breaks = scales::trans_breaks("log10", function(x) 10^x), labels = scales::trans_format("log10", scales::math_format(10^.x))) +
   scale_y_log10(breaks = scales::trans_breaks("log10", function(x) 10^x), labels = scales::trans_format("log10", scales::math_format(10^.x))) +
-  theme_bw(base_size = 11) + geom_point(shape=1,size=3)
+  theme_bw(base_size = 11) + geom_point(shape=1,size=3) + ylab("Frequency (no. of days)")
 
 print(pl)
