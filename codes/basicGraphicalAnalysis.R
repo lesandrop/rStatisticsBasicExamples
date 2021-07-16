4dea39a160a6371fe7a890ba0b4e6289c05dc26d
Lesandro Ponciano <lesandropcodes@users.noreply.github.com>
2021-07-16 16:59:21 -0300

    Update basicGraphicalAnalysis.R
    Co-Authored-By: Lesandro Ponciano <2512365+lesandrop@users.noreply.github.com>
    Co-Authored-By: Lesandro Ponciano <7714190+lesandropcodes@users.noreply.github.com>

--- codes/basicGraphicalAnalysis.R
+++ codes/basicGraphicalAnalysis.R
@@ -1,19 +1,25 @@
-#vetores
-ages <- c(17,18,19,18,17,20,17,18,20,18)
-height <- c(1.7,1.8,1.9,1.8,1.7,1.8,1.7,1.8,1.7,1.8)
-df_students <- data.frame(Age=ages, Height=height)
+#scenario
+scenario <- c(1, 2, 3, 4, 5, 6, 7, 8, 9)
+#Run time vectors
 
-#bars
-plot(df_students)
+strategy1 <- c(11, 16, 10, 4, 8, 13, 17, 18, 5)
+strategy2 <- c(14, 17, 9, 5, 7, 11, 19, 21, 4)
+expRawResults <- data.frame(Strategy1=strategy1, Strategy2=strategy2)
 
-#points
-plot(Age ~ Height, data=df_students)
-plot(Age ~ c(1:10), data=df_students)
+#histogram of strategy1
+hist(strategy1, ylab="Run time", xlab="Strategy", main="")
 
-#boxplot
-boxplot(df_students)
-boxplot(df_students$Age)
-boxplot(df_students$Height)
+#histogram of strategy2
+hist(strategy2, ylab="Run time", xlab="Strategy", main="")
 
-#hist
-hist(df_students$Height, ylab="Frequência", xlab="Altura", main="")
+#boxplot of both strategy1 and strategy2
+boxplot(expRawResults, xlab="Strategy", ylab="Run time", main="")
+
+
+#Comparing strategy1 and strategy2 lines per scenario
+plot(strategy1 ~ scenario, type = "b", frame = FALSE, pch = 19, col = "red", xlab = "x", ylab = "y")
+lines(strategy2 ~ scenario, pch = 18, col = "blue", type = "b", lty = 2)
+legend("bottomleft", legend=c("Strategy 1", "Strategy 2"), col=c("red", "blue"), lty = 1:2, cex=0.8)
+
+#Comparing strategy1 and strategy2 points
+plot(strategy1~strategy2, xlab="Strategy 2", ylab="Strategy 1", main="")
